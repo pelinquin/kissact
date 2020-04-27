817d7dd80072af1ccc4e2bcd8d8d3591b504cc48
Laurent Fournier <pelinquin@gmail.com>
2020-04-27 16:13:24 +0200

    Debt limit check

--- kissact.py
+++ kissact.py
@@ -4,15 +4,16 @@
 """
 
 import secrets, random, operator, ecc
-PREFIX = b'\x19'
+PREFIX  = b'\x19'
+MAXDEBT = 100
 
 def contact(u1, u2, duration=1, proximity=1, price=0):
     " 1B:PREFIX + 12B:EphIds + 2B:Hash + 1B:Price -> 16B BLEid "
     if price > 0:  # Generalized transaction with ECOTAX: 20%
         u1.ids[-1][0] = u1.ids[-1][0][:-3] + u2.ids[-1][0][1:3] + ecc.i2b(price, 1)
         u2.bal, u1.bal = u2.bal + price*4//5, u1.bal - price
-        u1.note[u1.ids[-1][0]] = (u1.pk, u1.k.sign(u1.ids[-1][0])) # (public key, signature)
-        u2.refe[u1.ids[-1][0]] = True
+        u1.note[u1.ids[-1][0]] = (u1.pk[:8], u1.k.sign(u1.ids[-1][0])) # (public id, signature)
+        u2.refe[u1.ids[-1][0]] = u2.pk[:8] # or False
     u1.cts[u2.ids[-1][0]] = (len(u1.ids), duration, proximity)
     u2.cts[u1.ids[-1][0]] = (len(u1.ids), duration, proximity)
     u1.ids[-1][1][2] = u2.ids[-1][1][2] = True 
@@ -42,8 +43,8 @@ class ctApp:
         for x in self.cts: print ('  ', ecc.b2h(x[1:]), self.cts[x])
 
     def pretest(self, s):
-        for x in self.note.keys(): s.house[0][x] = self.note[x]
-        for x in self.refe.keys(): s.house[1][x] = True
+        for x in self.note.keys(): s.hse[0][x] = self.note[x]
+        for x in self.refe.keys(): s.hse[1][x] = self.refe[x]
 
     def test(self, s):
         " Updates parametric model "
@@ -72,7 +73,7 @@ class ctServer:
     " Public backend "
     def __init__(self, param):
         self.inf, self.tick, self.coef, self.tax, self.pks = [], 0, param, 0, {}
-        self.house = [{}, {}]
+        self.hse = [{}, {}]
     def register(self, u):
         for x in u: self.pks[x.k.compress(x.k.pt)] = True
     def declare(self, ids):
@@ -85,14 +86,19 @@ class ctServer:
             for x in u: x.next()
     def test(self, u):
         for x in u: x.pretest(self)        
-        self.tax = sum([ecc.b2i(x[-1:])//5 for x in self.house[0]])
+        self.tax, k, tab = sum([ecc.b2i(x[-1:])//5 for x in self.hse[0]]), ecc.ecdsa(), {}
         for x in u: x.test(self)
-        assert operator.eq(self.house[0].keys(), self.house[1].keys())
-        assert sum([x.bal for x in u]) + self.tax == 0
-        for x in self.house[0]:
-            k = ecc.ecdsa()
-            k.pt = k.uncompress(self.house[0][x][0])
-            assert k.verify(self.house[0][x][1], x)
+        assert operator.eq(self.hse[0].keys(), self.hse[1].keys()) # VERIF 1: no leaks
+        assert sum([x.bal for x in u]) + self.tax == 0 # VERIF 2: sum balances null
+        for x in self.hse[0]:
+            for z in [y for y in self.pks if y[:8] == self.hse[0][x][0]]: k.pt = k.uncompress(z)
+            assert k.verify(self.hse[0][x][1], x) # VERIF 3: Transaction signature ok
+        for x in self.hse[0]:
+            y, pr = self.hse[0][x][0], ecc.b2i(x[-1:])
+            tab[y] = tab[y] + pr if y in tab else pr
+        for z in [x for x in self.hse[1] if self.hse[1][x] in tab]: tab[self.hse[1][z]] -= ecc.b2i(z[-1:])
+        for t in tab: assert tab[t] < MAXDEBT # VERIF 4: all debt under limit
+            
     def log(self, u):
         print (__doc__, __file__, sum(1 for l in open(__file__)), 'lines')
         for x in u: x.log()
@@ -108,14 +114,16 @@ if __name__ == "__main__":
     s.next_epoch(u, 3)                      # 3 steps
     contact(u[1], u[2], 50, 20)             # Bob meet Carol
     s.next_epoch(u, 5)                      # 5 steps
-    contact(u[2], u[1], 300, 40, 20)        # Carol pays 20 to Bob
+    contact(u[2], u[1], 300, 40, price=20)  # Carol pays 20 to Bob
     s.next_epoch(u, 2)                      # 2 steps
     s.declare(u[2].filter(True))            # Carol is sick and gives a subset of her EphIds
     s.declare(u[3].filter())                # David is sick and gives all EphIds
     s.next_epoch(u, 4)                      # 4 steps
-    contact(u[3], u[2], 100, 10, 10)        # David pays 10 to Carol
+    contact(u[3], u[2], 100, 10, price=10)  # David pays 10 to Carol
+    s.next_epoch(u, 1)                      # 1 step1
+    contact(u[3], u[2], 100, 10, price=5)   # David pays 5 to Carol again
     s.next_epoch(u, 2)                      # 2 steps
     s.log(u)                                # log display
     s.test(u)                               # only Bob got a risk warning
-    # The sum of all balances is always null
-# end
+
+# End ⊔net!
